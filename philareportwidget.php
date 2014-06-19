7a3ac4d730cb4a0ae4c63cd1d9410f79d057586a
Andrew Kennel <andrew.kennel@phila.gov>
2014-06-19 14:46:53 -0400

    Replaced hard-coded styles. removed 311 link

--- philareportwidget.php
+++ philareportwidget.php
@@ -6,72 +6,30 @@ Version: 1.0
 Author: Andrew Kennel
 Author URI: localhost/wordpress
 */
+add_shortcode('PhilaReportWidget', 'philareport_handler');
 
-class PhilaReportWidget extends WP_Widget {
-          function PhilaReportWidget() {
-                    $widget_ops = array(
-                    'classname' => 'PhilaReportWidget',
-                    'description' => 'Phila Report Widget.'
-          );
+function philareport_handler(){
+$message = <<<EOM
 
-          $this->WP_Widget(
-                    'PhilaReportWidget',
-                    'Phila Report Widget',
-                    $widget_ops
-          );
-}
-
-          function widget($args, $instance) { // widget sidebar output
-                    extract($args, EXTR_SKIP);
-                    echo $before_widget; // pre-widget code from theme
-print <<<EOM
-
-<div id="ReportWidget" style="background-color: #FAFAFA; border-radius: 10px; padding: 10px">
-<div class="bluebox" style="background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#3661a2), color-stop(100%,#162e52)); margin-bottom: 5px; border: 1px solid #3661a2; border-radius: 10px; width: 70%; padding: 10px 3px 0px 10px; height: 28px;">
-        <a href="http://www.phila.gov/ig/Pages/ReportWrongdoing.aspx" style="color:#ffffff; font-size: 15px; font-weight: bold; text-decoration:none">Fraud/Corruption</a><div class="triangle" style="font-size: 0px; 
-     height: 0; 
-     width: 0;
-     border-top: 10px solid transparent;
-     border-bottom: 10px solid transparent;
-     border-left: 10px solid #ffffff; float: right"></div>
-        </div>
-        
-        <div class="bluebox" style="background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#3661a2), color-stop(100%,#162e52)); margin-bottom: 5px; border: 1px solid #3661a2; border-radius: 10px; width: 70%; padding: 10px 3px 0px 10px; height: 28px;">
-        <a href="http://phillypolice.com/forms/submit-a-tip" style="color:#ffffff; font-size: 15px; font-weight: bold; text-decoration:none">Submit a Tip</a><div class="triangle" style="font-size: 0px; 
-     height: 0; 
-     width: 0;
-     border-top: 10px solid transparent;
-     border-bottom: 10px solid transparent;
-     border-left: 10px solid #ffffff; float: right"></div>
-        </div>
-        
-        <div class="bluebox" style="background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#3661a2), color-stop(100%,#162e52)); margin-bottom: 5px; border: 1px solid #3661a2; border-radius: 10px; width: 70%; padding: 10px 3px 0px 10px; height: 28px;">
-        <a href="http://potholes.phila.gov/tap.nsf/d15d032a30a36768852578850008337c?OpenForm" style="color:#ffffff; font-size: 15px; font-weight: bold; text-decoration:none">Potholes</a><div class="triangle" style="font-size: 0px; 
-     height: 0; 
-     width: 0;
-     border-top: 10px solid transparent;
-     border-bottom: 10px solid transparent;
-     border-left: 10px solid #ffffff; float: right"></div>
-        </div>
-        
-        
-        <div class="bluebox" style="background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#3661a2), color-stop(100%,#162e52)); margin-bottom: 5px; border: 1px solid #3661a2; border-radius: 10px; width: 70%; padding: 10px 3px 0px 10px; height: 28px;">
-        <a href="http://www.phila.gov/311" style="color:#ffffff; font-size: 15px; font-weight: bold; text-decoration:none">Philly 311</a><div class="triangle" style="font-size: 0px; 
-     height: 0; 
-     width: 0;
-     border-top: 10px solid transparent;
-     border-bottom: 10px solid transparent;
-     border-left: 10px solid #ffffff; float: right"></div>
-        </div>
+<div id="PhilaReportWidget" class="PhilaWidget">
+	<span id="PhilaReportMainWindow">
+		<h1>Talk to your City</h1>	
+		 <a href="http://www.phila.gov/ig/Pages/ReportWrongdoing.aspx">Report Fraud/Corruption</a>
+		 <br>
+		 <a href="http://phillypolice.com/forms/submit-a-tip">Submit a Police Tip</a>
+		 <br>
+		 <a href="http://potholes.phila.gov/tap.nsf/d15d032a30a36768852578850008337c?OpenForm">Report a Pothole</a>		 
+	</span>
 </div>
 
 EOM;
-                    echo $after_widget; // post-widget code from theme
-          }
+return $message;
 }
 
-add_action(
-          'widgets_init',
-          create_function('','return register_widget("PhilaReportWidget");')
-);
+function philareportwidget($args, $instance) { // widget sidebar output
+  extract($args, EXTR_SKIP);
+  echo $before_widget; // pre-widget code from theme
+  echo philareport_handler();
+  echo $after_widget; // post-widget code from theme
+}
 ?>
